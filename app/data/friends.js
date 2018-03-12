0cd6a3b8cb36b1267f886d533886303651deec64
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-03-12 18:12:41 -0500

    updated pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsArr = [{
     name: "Jerry",
-    photo: "https://villains.wikia.com/wiki/Jerry_the_Mouse?file=Jerrythemouse.jpg",
+    photo: "https://vignette.wikia.nocookie.net/tomandjerry/images/5/5f/JerryMouse.png/revision/latest?cb=20140723114908",
     scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
 }];
 
